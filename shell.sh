94af0187d88a17bcd1f4c1882de647d6bc128e1a
HoJianLin <82578605+HoJianLin@users.noreply.github.com>
2023-03-24 00:10:16 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -22,9 +22,6 @@ function mysqlinstall (){
 	yum -y install mysql-community-libs-compat-5.7.36-1.el7.x86_64.rpm;
 	yum -y install mysql-community-client-5.7.36-1.el7.x86_64.rpm;
 	yum -y install mysql-community-server-5.7.36-1.el7.x86_64.rpm;
-	rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022;
-	yum -y install mysql57-community-release-el7-10.noarch.rpm;
-	yum -y install mysql-community-server;
 	systemctl start mysqld.service;
 }
 function phpinstall(){
