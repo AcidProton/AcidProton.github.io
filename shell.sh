deead5f965e05dbc4c46e654b21b09a4f24db2a3
HoJianLin <82578605+HoJianLin@users.noreply.github.com>
2023-03-24 15:59:02 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -14,11 +14,11 @@ function mysqldownload (){
 }
 function mysqlinstall (){
 	wget http://dev.mysql.com/get/mysql57-community-release-el7-10.noarch.rpm;
-	yum -y install mysql-community-common-5.7.36-1.el7.x86_64.rpm;
-	yum -y install mysql-community-libs-5.7.36-1.el7.x86_64.rpm;
-	yum -y install mysql-community-libs-compat-5.7.36-1.el7.x86_64.rpm;
-	yum -y install mysql-community-client-5.7.36-1.el7.x86_64.rpm;
-	yum -y install mysql-community-server-5.7.36-1.el7.x86_64.rpm;
+	yum localinstall mysql-community-common-5.7.36-1.el7.x86_64.rpm;
+	yum localinstall mysql-community-libs-5.7.36-1.el7.x86_64.rpm;
+	yum localinstall mysql-community-libs-compat-5.7.36-1.el7.x86_64.rpm;
+	yum localinstall mysql-community-client-5.7.36-1.el7.x86_64.rpm;
+	yum localinstall mysql-community-server-5.7.36-1.el7.x86_64.rpm;
 	rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022;
 	yum -y install mysql57-community-release-el7-10.noarch.rpm;
 	yum -y install mysql-community-server-5.7.36;
