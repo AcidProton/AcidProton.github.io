a91a377acdaa857131a18255f9ec4f286485d5c9
HoJianLin <82578605+HoJianLin@users.noreply.github.com>
2023-03-24 09:54:10 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -7,19 +7,19 @@ function httpdinstall (){
 function mysqldownload (){
 	yum install -y libaio &
 	wget http://mirrors.ustc.edu.cn/mysql-ftp/Downloads/MySQL-5.7/mysql-community-common-5.7.36-1.el7.x86_64.rpm;
-	yum remove mariadb-libs;
-	yum -y install mysql-community-common-5.7.36-1.el7.x86_64.rpm &
 	wget http://mirrors.ustc.edu.cn/mysql-ftp/Downloads/MySQL-5.7/mysql-community-libs-5.7.36-1.el7.x86_64.rpm;
-	yum -y install mysql-community-libs-5.7.36-1.el7.x86_64.rpm &
 	wget http://mirrors.ustc.edu.cn/mysql-ftp/Downloads/MySQL-5.7/mysql-community-libs-compat-5.7.36-1.el7.x86_64.rpm;
-	yum -y install mysql-community-libs-compat-5.7.36-1.el7.x86_64.rpm &
 	wget http://mirrors.ustc.edu.cn/mysql-ftp/Downloads/MySQL-5.7/mysql-community-client-5.7.36-1.el7.x86_64.rpm;
-	yum -y install mysql-community-client-5.7.36-1.el7.x86_64.rpm &
 	wget http://mirrors.ustc.edu.cn/mysql-ftp/Downloads/MySQL-5.7/mysql-community-server-5.7.36-1.el7.x86_64.rpm;
-	yum -y install mysql-community-server-5.7.36-1.el7.x86_64.rpm &
 	wget http://dev.mysql.com/get/mysql57-community-release-el7-10.noarch.rpm;
 }
 function mysqlinstall (){
+	yum remove mariadb-libs;
+	yum -y install mysql-community-common-5.7.36-1.el7.x86_64.rpm;
+	yum -y install mysql-community-libs-5.7.36-1.el7.x86_64.rpm;
+	yum -y install mysql-community-libs-compat-5.7.36-1.el7.x86_64.rpm;
+	yum -y install mysql-community-client-5.7.36-1.el7.x86_64.rpm;
+	yum -y install mysql-community-server-5.7.36-1.el7.x86_64.rpm;
 	rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022;
 	yum -y install mysql57-community-release-el7-10.noarch.rpm;
 	yum -y install mysql-community-server-5.7.36;
@@ -35,9 +35,9 @@ function phpinstall(){
 }
 mysqldownload &
 httpdinstall &
+phpinstall &
 wait;
 mysqlinstall;
-phpinstall;
 systemctl restart httpd;
 pwline=$(grep "password" /var/log/mysqld.log);
 pw=${pwline##*:};
